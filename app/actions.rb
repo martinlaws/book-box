1def4caacf0d23fd0d94015f83dcb6d8d24597e8
jdbwin <dbwin7@gmail.com>
2015-11-11 16:47:46 -0500

    added secure password to user

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,35 @@
-# Homepage (Root path)
 get '/' do
   erb :index
+end
+
+helpers do
+
+  def current_user
+
+    if session[:id] and user = User.find(session[:id])
+      user
+    end
+
+  end
+end
+
+post '/signup' do
+
+  @user = User.new(
+    email: params[:email],
+    password: params[:password],
+    first_name: params[:first_name],
+    last_name: params[:last_name],
+    location: params[:location]
+  )
+
+  if user.save
+
+    redirect '/trade_wall'
+
+  else
+
+    erb:index
+
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  has_secure_password
+
   has_many :open_trades, class_name:'Trade', foreign_key: :posting_user_id
 
   has_many :completed_trades, class_name:'Trade', foreign_key: :receiving_user_id
