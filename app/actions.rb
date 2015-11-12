b78c46d978c011663e8ad81ef6659bb876cec060
jdbwin <dbwin7@gmail.com>
2015-11-12 11:18:30 -0500

    updated actions.rb

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,4 @@
+require 'pry'
 helpers do
 
   def current_user
@@ -5,34 +6,12 @@ helpers do
       user
     end
   end
+
 end
 
-get '/logout' do
-  session.clear
-  redirect "/login"
-end
-
-get '/login' do
-  erb :'users/login'
-end
-
-get '/trade_wall' do
-  erb :'users/trade_wall'
-end
-
-post '/login' do
-
-  if @user = User.find_by_email(params[:email]).try(:authenticate, params[:password])
-    session[:id] = @user.id
-
-    redirect "/trade_wall"
-
-  else
-
-    @error = "Wrong email/password"
-    erb :'users/login'
-
-  end
+before do
+  @flash = session[:flash]
+  session.delete(:flash)
 end
 
 get '/' do
@@ -45,25 +24,76 @@ get '/' do
 
 end
 
+get '/login' do
+  erb :'users/login'
+end
+
 get '/signup' do
   erb :'users/signup'
 end
 
+get '/logout' do
+
+  session.clear
+  redirect "/login"
+
+end
+
+get 'user/trade_wall' do
+  @books = Book.all
+
+  erb :'books/trade_wall'
+end
+
+post 'books/new' do
+
+  @book = Book.new(
+    title: params[:title],
+    author: params[:author],
+    genre: params[:genre],
+    #location: params[:location]
+  )
+
+  if @user.save
+    session[:id] = @user.id
+    session[:flash] = "Welcome!"
+    redirect '/trade_wall'
+  else
+    erb :index
+  end
+
+
+end
+
+post '/login' do
+
+  if @user = User.find_by_email(params[:email]).try(:authenticate, params[:password])
+    session[:id] = @user.id
+    redirect "/trade_wall"
+  else
+    @error = "Wrong email/password"
+    erb :'users/login'
+  end
+
+end
+
+
 post '/signup' do
 
   @user = User.new(
-
     email: params[:email],
     password: params[:password],
     first_name: params[:first_name],
     last_name: params[:last_name],
     #location: params[:location]
-
   )
 
   if @user.save
+    session[:id] = @user.id
+    session[:flash] = "Welcome!"
     redirect '/trade_wall'
   else
     erb :index
   end
+
 end
