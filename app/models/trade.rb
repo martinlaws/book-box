c111b579362aa9051453ef2e0f9577a1dbac0a96
jdbwin <dbwin7@gmail.com>
2015-11-13 16:30:10 -0500

    fixed validation for trade

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -3,6 +3,6 @@ class Trade < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
 
-  validates :receiving_user, presence: false
+  validates :receiving_user, uniqueness: {scope: :book_id}
 
 end
