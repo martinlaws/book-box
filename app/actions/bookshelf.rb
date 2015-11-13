63f86917f4afc6e36c6dbf0f4c86b1e19e264df4
jdbwin <dbwin7@gmail.com>
2015-11-13 16:21:02 -0500

    fixed availability button

--- app/actions/bookshelf.rb
+++ app/actions/bookshelf.rb
@@ -2,3 +2,19 @@ get '/bookshelf:id' do
   erb :'users/bookshelf'
 end
 
+put '/book:id/repost' do
+  @book = Book.find(params[:id])
+  @book.availability = true
+  @book.save
+
+  erb :'/users/bookshelf'
+end
+
+put '/book:id/remove' do
+  @book = Book.find(params[:id])
+  @book.availability = false
+  @book.save
+
+  erb :'/users/bookshelf'
+end
+
