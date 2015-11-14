db6188ae84e19cbaeef717a07a86da054cbf98f7
jdbwin <dbwin7@gmail.com>
2015-11-14 03:29:36 -0500

    updated to geocode gem

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
-  $(".getLocation").click(function() {
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    }
-    else {
-      console.log ("Not supported");
-    }
-
-    function showPosition(position) {
-      $(".latitude").val(position.coords.latitude);
-      $(".longitude").val(position.coords.longitude);
-    }
-  });
-
-  $(".trade-button").click(function() {
-  });
-});
+//$(document).ready(function() {
+//  $(".getLocation").click(function() {
+//    if(navigator.geolocation) {
+//      navigator.geolocation.getCurrentPosition(showPosition);
+//    }
+//    else {
+//      console.log ("Not supported");
+//    }
+//
+//    function showPosition(position) {
+//      $(".latitude").val(position.coords.latitude);
+//      $(".longitude").val(position.coords.longitude);
+//    }
+//  });
+//
+//  $(".trade-button").click(function() {
+//  });
+//});
