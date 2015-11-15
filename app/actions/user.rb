6215a6e46a3cf2b14416f6b3826808978c8314d3
Martin Laws <martinblaws@gmail.com>
2015-11-15 22:14:01 +0000

    added update user

--- app/actions/user.rb
+++ app/actions/user.rb
@@ -1,4 +1,19 @@
 get '/user/:id' do
   @user = User.find(params[:id])
-  erb :'user/show'
+  erb :'users/show'
+end
+
+get "/:id/edit" do
+  erb :'/users/edit_profile'
+end
+
+put "/:id/edit" do
+  @user = User.find(params[:id])
+  @user.update(
+    email: params[:email],
+    password: params[:password],
+    first_name: params[:first_name],
+    last_name: params[:last_name],
+    location: params[:location]
+  )
 end
