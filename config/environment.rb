77455314f6d13c5a8caa84e77e4eb7f9407e294c
Martin Laws <martinblaws@gmail.com>
2015-11-17 00:18:10 +0000

    oops

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/contrib/all' # Requires cookies, among other things
 
-require 'pry'
+require 'pry' if development?
 require 'geocoder'
 require 'geocoder/railtie'
 Geocoder::Railtie.insert
